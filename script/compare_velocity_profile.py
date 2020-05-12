20f7b1693043c592951e45938645010f7cfc2833
Unknown <415569252@qq.com>
2020-05-12 15:54:39 +0800

    添加仿真

--- script/compare_velocity_profile.py
+++ script/compare_velocity_profile.py
@@ -6,6 +6,7 @@ import message_filters
 from nav_msgs.msg import Odometry
 from diff_msgs.msg import WheelSpeedStatus
 import matplotlib.pyplot as plt
+import math
 
 # 全局变量
 store_path = '/home/flztiii/Documents/turtlebot_test/compare/'
@@ -16,8 +17,14 @@ velocitys = []
 last_odom = []
 
 # 消息回调函数
-def callback(odom_msg, diff_msg):
-    velocity = (diff_msg.leftWheelSpeed + diff_msg.rightWheelSpeed) / 2000.0
+def callback(odom_msg):
+    global store_path
+    global index
+    global distance
+    global distances
+    global velocitys
+    global last_odom
+    velocity = math.sqrt(odom_msg.twist.twist.linear.x ** 2 + odom_msg.twist.twist.linear.y ** 2)
     # 判断是不是第一个odom
     if len(last_odom) == 0:
         last_odom.append(odom_msg)
@@ -25,7 +32,7 @@ def callback(odom_msg, diff_msg):
         velocitys.append(velocity)
     else:
         # 如果不是第一个
-        distance += sqrt((odom_msg.pose.pose.position.x - last_odom[0].pose.pose.position.x) ** 2 + (odom_msg.pose.pose.position.y - last_odom[0].pose.pose.position.y) ** 2)
+        distance += math.sqrt((odom_msg.pose.pose.position.x - last_odom[0].pose.pose.position.x) ** 2 + (odom_msg.pose.pose.position.y - last_odom[0].pose.pose.position.y) ** 2)
         last_odom[0] = odom_msg
         distances.append(distance)
         velocitys.append(velocity)
@@ -42,13 +49,8 @@ def main():
     rospy.init_node('compare_velocity_profile_node', anonymous=True)
     # 订阅速度和定位信息
     odom_topic = '/planning_input/localization'
-    vel_topic = '/wheel_speed_status'
+    odom_sub = rospy.Subscriber(odom_topic, Odometry, callback=callback)
 
-    odom_sub = message_filters.Subscriber(odom_topic, Odometry)
-    vel_sub = message_filters.Subscriber(vel_topic, WheelSpeedStatus)
-
-    ts = message_filters.TimeSynchronizer([odom_sub, vel_sub], 10)
-    ts.registerCallback(callback)
     rospy.spin()
 
 if __name__ == "__main__":
